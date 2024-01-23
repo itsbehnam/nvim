26a217554e6155700b1f642e681b0c65d8d7f8c0
Behnam <behnam.h1@gmail.com>
2024-01-23 14:07:43 +0200

    prefixed <leader> for oil

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,7 +24,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
