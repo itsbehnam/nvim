55aa2f94cab91f79b1383f0382aadf3cead538bb
Behnam <behnam.h1@gmail.com>
2024-03-27 14:27:11 +0200

    indentation keymaps for visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,6 +24,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
 vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- [[ Highlight on yank ]]
